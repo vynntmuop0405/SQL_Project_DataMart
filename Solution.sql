0aa777cc1c3afb816277e6a3d52a081a85ce58e6
VyNguyen <nguyenngocthaovy451999@gmail.com>
2025-08-22 23:07:14 +0700

    value

--- Solution.sql
+++ Solution.sql
@@ -23,3 +23,32 @@ SELECT DISTINCT week_date_format,
 	   DATENAME(WEEKDAY, week_date_format) AS date_name,
 	   DATENAME(MONTH, week_date_format)
 FROM data_mart_clean_weekly_sales
+
+--How many total transactions were there for each year in the dataset?
+SELECT year_number, COUNT(*) num_of_trans FROM data_mart_clean_weekly_sales
+GROUP BY year_number
+
+year_number	num_of_trans
+2018		158
+2019		119
+2020		233
+
+--What is the total sales for each region for each month?
+SELECT region, month_number, year_number, SUM(sales) total_sales FROM data_mart_clean_weekly_sales
+GROUP BY  region, month_number, year_number
+
+--What is the total count of transactions for each platform
+SELECT platform, COUNT(*) num_of_trans FROM data_mart_clean_weekly_sales
+GROUP BY  platform
+
+platform	num_of_trans
+Retail		253
+Shopify		257
+
+--What is the percentage of sales for Retail vs Shopify for each month?
+SELECT year_number, month_number, platform,
+	   SUM(sales) total_sales,
+	   ROUND(100.0*SUM(sales)/ SUM(SUM(sales)) OVER(PARTITION BY year_number, month_number),2) AS percentage_sales
+FROM data_mart_clean_weekly_sales
+GROUP BY year_number, month_number, platform
+ORDER BY year_number, month_number, platform
